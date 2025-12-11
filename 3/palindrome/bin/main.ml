564a34569f8d9bfec3ead189c8c29352d962bee5
andrea-sunda <andreasunda05@gmail.com>
2025-12-11 17:56:51 +0100

    Palindrome

---
+++ 3/palindrome/bin/main.ml
@@ -0,0 +1,16 @@
+(* re-use of ex 5 *)
+let rec rev lst =
+  match lst with
+  | [] -> lst
+  | h :: t -> rev(t) @ [h]
+;;
+
+let is_palindrome lst =
+  lst = rev lst
+;;
+
+assert(is_palindrome []);;
+assert(is_palindrome ['a';'n';'n';'a']);;
+assert(is_palindrome ['r';'a';'d';'a';'r']);;
+assert(is_palindrome ['a';'n';'n';'e'] = false);;
+assert(is_palindrome ['z';'a';'n';'n';'a'] = false);;
