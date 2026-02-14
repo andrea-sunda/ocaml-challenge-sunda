6cb808d5784359b4f9da64ecd6016b6d16bf4aa9
andrea-sunda <andreasunda05@gmail.com>
2026-02-14 02:50:05 +0100

    Simple language recognizers

---
+++ 3/lang-recognizers/bin/main.ml
@@ -0,0 +1,65 @@
+let rec l0 = function
+  | [1] -> true
+  | 0 :: t -> l0 t
+  | 1 :: t -> l0 t
+  | _ -> false
+;;
+
+let l1 lst =
+  let rec no_zero = function
+    | [] -> true
+    | 0 :: _ -> false
+    | 1 :: t ->  no_zero t
+    | _ -> false
+  in
+  let rec f = function
+    | [] -> true
+    | 1 :: t -> f t
+    | 0 :: t -> no_zero t 
+    | _ -> false
+  in
+  f lst
+;;
+
+let l2 lst =
+  let rec no_zero = function
+    | [] -> true
+    | 0 :: _ -> false
+    | (1 | 2) :: t ->  no_zero t
+    | _ -> false
+  in
+  let rec f = function
+    | [] -> true
+    | 1 :: t -> no_zero t
+    | (0 | 2) :: t -> f t
+    | _ -> false
+  in
+  f lst
+;;
+
+let rec l3 = function
+  | [] -> true
+  | 0 :: 1 :: 1 :: t -> l3 t
+  | 0 :: _ -> false
+  | _ :: t -> l3 t
+;;
+
+let l4 lst =
+  let rec count_zeros_ones zeros ones = function
+    | [] -> zeros >= ones
+    | 0 :: t -> count_zeros_ones (zeros+1) ones t
+    | 1 :: t -> count_zeros_ones zeros (ones+1) t
+    | _ -> false
+  in
+  count_zeros_ones 0 0 lst
+;;
+
+let l4 lst =
+  let rec count_zeros_ones zeros ones = function
+    | [] -> zeros = ones
+    | 0 :: t -> count_zeros_ones (zeros+1) ones t
+    | 1 :: t -> count_zeros_ones zeros (ones+1) t
+    | _ -> false
+  in
+  count_zeros_ones 0 0 lst
+;;
